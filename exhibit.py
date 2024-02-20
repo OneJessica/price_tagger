76fb71bcf1cd43eb5a6fcb3d699ef7f9a61c6499
zhangjin <54884562+OneJessica@users.noreply.github.com>
2024-02-20 22:19:17 +0800

    Update exhibit.py

--- exhibit.py
+++ exhibit.py
@@ -136,7 +136,7 @@ def main():
     with tab4:
         ## 库存搜索功能
         @st.cache_resource
-        def get_data(added = False):
+        def get_data():
             data = pd.read_csv('产品数据全量.csv',index_col = 0,keep_default_na=0)
             data = data.drop_duplicates(subset=['货号','通用名'])
             added_data = pd.read_csv('added_data.csv',header =None,index_col = 0,keep_default_na=0)
